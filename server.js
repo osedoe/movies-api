d231df410c781ccf7939c564e140af5bf607088e
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-11-12 18:02:54 +0000

    Express project with best practices

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const files = require("./src/utils/files");
 const app = express();
 const moviesRouter = require('./src/api/movies');
-
+const port = 3000;
 let movies;
 files.loadMovies(moviesData => movies = moviesData);
 
@@ -15,5 +15,5 @@ app.get('/', (req, res) => {
 app.use('/movies', moviesRouter);
 
 app.listen(3000, () => {
-  console.log('Ready on port 3000!');
+  console.log(`Ready on port ${port}!`);
 });
