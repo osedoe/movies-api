234a9141927c921ebf01c2564fd3348126dfafef
Yodra <yodralopez@gmail.com>
2018-11-12 20:02:25 +0000

    refactor save and fix get movies..

--- src/api/movies/controller.js
+++ src/api/movies/controller.js
@@ -1,70 +1,63 @@
-function getMovies() {
+const files = require("./../../utils/files");
+let movies = files.loadMovies(moviesData => movies = moviesData);
+
+function getMovies(req, res) {
   res.json(movies);
 }
 
-function getMovie() {
+function getMovie(req, res) {
   const movieId = req.params.id;
   const movie = movies.find(movie => movie.id === movieId);
   res.json(movie);
 }
 
-function getLike() {
+function getLike(req, res) {
   const likeMovies = movies.filter(movie => movie.like === true);
   res.json(likeMovies);
 }
 
-function postMovie() {
+function postMovie(req, res) {
   const movie = req.body;
   movie.id = `${movies.length + 1}`;
   movies.push(movie);
 
-  files.saveMovies(movies, err => {
-    if (err) {
-      res.error(err);
-    } else {
-      res.json(movies);
-    }
-  });
+  saveMovies(res);
 }
 
-function updateMovie() {
+function updateMovie(req, res) {
   const movieId = req.body.id;
   let moviePosition = movies.findIndex(movie => movie.id === movieId);
+
   if (moviePosition >= 0) {
     movies[moviePosition] = req.body;
   }
 
-  files.saveMovies(movies, err => {
-    if (err) {
-      res.error(err);
-    } else {
-      res.json(movies);
-    }
-  });
+  saveMovies(res);
 }
 
-function deleteMovie() {
+function deleteMovie(req, res) {
   const movieId = req.params.id;
   const moviePosition = movies.findIndex(movie => movie.id === movieId);
+
   if (moviePosition >= 0) {
     movies.splice(moviePosition, 1);
   }
 
-  files.saveMovies(movies, err => {
-    if (err) {
-      res.error(err);
-    } else {
-      res.json(movies);
-    }
-  });
+  saveMovies(res);
 }
 
-function deleteLike() {
+function deleteLike(req, res) {
   const movieId = req.params.id;
   const movie = movies.find(movie => movie.id === movieId);
+
   if (movie) {
     movie.like = false;
   }
+
+  saveMovies(res);
+}
+
+function saveMovies(res) {
   files.saveMovies(movies, err => {
     if (err) {
       res.error(err);
@@ -74,4 +67,4 @@ function deleteLike() {
   });
 }
 
-module.exports = { getMovie, getMovies, getLike, postMovie, updateMovie, deleteMovie, deleteLike };
+module.exports = {getMovie, getMovies, getLike, postMovie, updateMovie, deleteMovie, deleteLike};
